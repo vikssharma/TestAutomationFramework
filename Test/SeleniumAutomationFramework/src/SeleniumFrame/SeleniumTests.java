b808a9ea5ef2db4cdd90139ca4df55248c9ac756
vikssharma <vikscoolster@gmail.com>
2015-09-18 13:14:55 +0530

    Update SeleniumTests.java

--- Test/SeleniumAutomationFramework/src/SeleniumFrame/SeleniumTests.java
+++ Test/SeleniumAutomationFramework/src/SeleniumFrame/SeleniumTests.java
@@ -1,11 +1,44 @@
 package SeleniumFrame;
 
-import org.testng.annotations.Test;
+import java.io.File;
+import java.io.IOException;
 
-public class SeleniumTests {
-  @Test
-  public void f() {
-	  System.out.println("Hello Automation Framework");
-	  System.out.println("Hello Automation");
-  }
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.Assert;
+
+
+public class OS_Version {
+	
+//	@Test
+//	public void firsttest() throws IOException {
+//	    Driver.TestSuite("Driver");
+//	    FunctionLibrary.WaitForObject(1000);
+//        FunctionLibrary.CloseDriver();
+//    }
+	
+	@Before
+	public void Secondtest() throws IOException {
+	    FunctionLibrary.PerformAction("LaunchIE", "", "", "http://google.com");
+    }
+	
+	@Test
+	public void Thirdtest() throws IOException {
+	    FunctionLibrary.EnterText("q", "Selenium");
+	    FunctionLibrary.WaitForObject(10000);
+	    FunctionLibrary.PerformClick("btnG", "Name");
+	    Assert.assertEquals("Selenium",FunctionLibrary.GetObjectText("q"));
+	    FunctionLibrary.screenshot("Screen1");
+	    FunctionLibrary.PerformClick("//*[@id='rso']/div[1]/div[1]/div/h3/a", "Xpath");
+	    FunctionLibrary.PerformClick("//*[@id='menu_download']/a", "Xpath");
+	    FunctionLibrary.screenshot("Screen2");
+	}
+	
+	@After
+	public void Closetest() throws IOException {
+	    FunctionLibrary.CloseDriver();
+    }
+	
 }
